145f30d3a1aadfbbacd47781eacd96d2fab32a55
reedanj <james.r@unitaskr.com>
2024-04-16 07:46:02 +0100

    add project -card

---
+++ src/app/project-card/project-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-project-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-card.component.html',
+  styleUrl: './project-card.component.scss'
+})
+export class ProjectCardComponent {
+  @Input() projectName: string = '';
+}
