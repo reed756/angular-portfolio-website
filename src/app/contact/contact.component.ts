29dbc7c23edec80e3ae0f9507c764314ae762516
reedanj <james.r@unitaskr.com>
2024-04-20 11:43:29 +0100

    fill out contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
 
   constructor(private title: Title) {
     this.title.setTitle('James Reed - Contact');
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
 }
