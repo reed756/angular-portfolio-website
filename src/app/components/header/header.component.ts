040eef3e2e102aeda66094877f10e5b7ff2f05f5
reedanj <james.r@unitaskr.com>
2025-03-23 12:44:03 +0000

    add hashnode link

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faHashnode } from '@fortawesome/free-brands-svg-icons';
 @Component({
-    selector: 'app-header',
-    imports: [FontAwesomeModule],
-    templateUrl: './header.component.html',
-    styleUrl: './header.component.scss'
+  selector: 'app-header',
+  imports: [FontAwesomeModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   faGithub = faGithub;
   faLinkedin = faLinkedin;
+  faHashnode = faHashnode;
 }
